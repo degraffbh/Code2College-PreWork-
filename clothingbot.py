0878742b4fdefff52f75873a0c69f71919ca172e
coolbe22 <benjamindegraff@gmail.com>
2024-11-06 22:00:56 +0000

    clothing bot base complete

--- clothingbot.py
+++ clothingbot.py
@@ -1,9 +1,22 @@
 print("Welcome to Ben's Clothing Store, how may we help you? \n1. Return an item\n2. Exhange and item.")
-answer = int(input("Please enter the number of your choice: "))
-
+validAnswer = False
+while validAnswer == False:
+    validAnswer = True
+    try:
+        answer = int(input("Please enter the number of your choice: "))
+        if answer != 1 and answer != 2:
+            validAnswer = False
+    except:
+        validAnswer = False
+    
 def response(answer):
     if answer == 1:
-        print("Return an item")
+        item = input("What item would you like to return? ")
+        print(f"{item} has been returned. Thank you for shopping with us.")
+
     elif answer == 2:
-        print("Exhange an item")    
+        oldItem = input("What item would you like to exchange? ")
+        newItem = input("What item would you like to recieve? ") 
+        print(f"{oldItem} has been returned and you have recieved {newItem} instead. Thank you for shopping with us.")
+
 response(answer)
